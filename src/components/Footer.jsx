85548055ac42b7fe6af4fa83686cbdc6ff1add7e
Sanukadg <154535969+Sanukadg@users.noreply.github.com>
2025-01-24 19:16:28 +0530

    Add functionality to Contact button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import styles from "./footer.module.css";
 
 export default function Footer() {
     return (
-        <div className={styles.container}>
-            <h2 id={styles.logo}>sanukadg</h2>
+        <div className={styles.container} id="footer">
+            <h2 id={styles.footer_logo}>sanukadg</h2>
             <p> Contact me! </p>
             <div className={styles.button_container}>
                 <div className={styles.button}>Email</div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,21 @@ import styles from "./nav.module.css";
 import Links from "./sub-components/Links";
 
 export default function Nav() {
+    const scrollToFooter = () => {
+        const footer = document.getElementById('footer');
+        if (footer) {
+            footer.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         <div className={styles.container}>
             <h1 id={styles.logo}>sanukadg</h1>
             <div className={styles.spacer}></div>
             <Links />
-            <div className={styles.contact_button}>Contact</div>
+            <div className={styles.contact_button} onClick={scrollToFooter}>
+                Contact
+            </div>
         </div>
     );
 }
