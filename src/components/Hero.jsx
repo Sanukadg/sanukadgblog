aebf9bd44458609ed996de431e174205c3fd1278
Sanukadg <154535969+Sanukadg@users.noreply.github.com>
2025-01-24 19:37:46 +0530

    fetch data from json file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import styles from "./hero.module.css";
 
 export default function Hero() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("./src/data/data.json"); // Fetching the properties data from the JSON file
+                if (!response.ok) throw new Error("Failed to fetch JSON");
+                const json = await response.json();
+                setData(json.posts);
+            } catch (error) {
+                console.error("Error fetching data:", error); // Log any errors
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className={styles.main_container}>
             <div className={styles.container1}>
@@ -9,16 +26,18 @@ export default function Hero() {
             </div>
             <div className={styles.container2}>
                 <div className={styles.title_container}>
-                    <p className={styles.title}>
-                        Simulator racing or Arcade racing
-                    </p>
-                    <p className={styles.title}>
-                        "Vertex Ridge" FD RX7 D1 project by A'pexi
-                    </p>
+                    {data.map((post) => (
+                        <div key={post.id} className={styles.title_container}>
+                            <p className={styles.title}>{post.title}</p>
+                        </div>
+                    ))}
                 </div>
                 <div className={styles.date_container}>
-                    <p className={styles.title_date}>Jul 16</p>
-                    <p className={styles.title_date}>Jun 7</p>
+                    {data.map((post) => (
+                        <div key={post.id} className={styles.title_container}>
+                            <p className={styles.title_date}>{post.date}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
