6b5de943e3b08ac90906c94ea47fc0bd0c458543
Sanukadg <154535969+Sanukadg@users.noreply.github.com>
2025-01-24 19:44:36 +0530

    Add footer Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,18 @@ export default function Footer() {
             <h2 id={styles.footer_logo}>sanukadg</h2>
             <p> Contact me! </p>
             <div className={styles.button_container}>
-                <div className={styles.button}>Email</div>
-                <div className={styles.button}>Github</div>
-                <div className={styles.button}>Twitter / X</div>
+                <div className={styles.button}>
+                    <a href="mailto:sanukagamage@gmail.com">Email</a>
+                </div>
+                <div className={styles.button}>
+                    <a href="https://github.com/Sanukadg">Github</a>
+                </div>
+                <div className={styles.button}>
+                    <a href="https://x.com/Onev82">Twitter / X</a>
+                </div>
+                <div className={styles.button}>
+                    <a href="https://instagram.com/_sg.media">Instagram</a>
+                </div>
             </div>
             <img src="/settingsico.svg" id={styles.icon} />
         </div>
