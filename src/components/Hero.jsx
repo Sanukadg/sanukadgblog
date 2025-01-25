c59b18a88f13228ba631d452d527d3bde46c5ab9
Sanukadg <154535969+Sanukadg@users.noreply.github.com>
2025-01-25 13:49:05 +0530

    new images with transition

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,80 @@
 import styles from "./hero.module.css";
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 export default function Hero({ data }) {
+    const images = [
+        {
+            src: import.meta.env.BASE_URL + "/nichijou.png",
+            style: {
+                maxWidth: "655px",
+                maxHeight: "370px",
+                left: "55%",
+            },
+        },
+        {
+            src: import.meta.env.BASE_URL + "/nichijou2.png",
+            style: {
+                maxWidth: "355px",
+                maxHeight: "370px",
+                left: "65%",
+            },
+        },
+        {
+            src: import.meta.env.BASE_URL + "/nichijou3.png",
+            style: {
+                maxWidth: "455px",
+                maxHeight: "570px",
+                left: "68%",
+            },
+        },
+        {
+            src: import.meta.env.BASE_URL + "/nichijou4.png",
+            style: {
+                maxWidth: "555px",
+                maxHeight: "310px",
+                left: "70%",
+            },
+        },
+        {
+            src: import.meta.env.BASE_URL + "/nichijou5.png",
+            style: {
+                maxWidth: "555px",
+                maxHeight: "310px",
+                left: "67%",
+            },
+        },
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 3000); // Change image every 3 seconds
+
+        return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, [images.length]);
+
     return (
         <div className={styles.main_container}>
             <div className={styles.container1}>
                 <p className={styles.hero_title}>Hello World!</p>
-                <img
-                    src={import.meta.env.BASE_URL + "/nichijou.png"}
-                    className={styles.hero_image}
-                />
+
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image.src}
+                        style={{
+                            position: "absolute",
+                            zIndex: "0",
+                            opacity: currentIndex === index ? 1 : 0,
+                            objectFit: "cover",
+                            transition: "opacity 0.35s ease-in-out",
+                            ...image.style,
+                        }}
+                    />
+                ))}
             </div>
             <div className={styles.container2}>
                 {data.map((post) => (
